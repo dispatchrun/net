f390d975c13cf5e2f65897c951e23125994e2efb
Achille Roussel <achille.roussel@gmail.com>
2023-06-09 11:49:15 -0700

    verify the version returned by the mysql server
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- mysql/mysql_wasip1_test.go
+++ mysql/mysql_wasip1_test.go
@@ -6,6 +6,8 @@ import (
 	"database/sql"
 	"testing"
 
+	// Importing this package configures go-sql-driver/mysql to use the dialer
+	// from github.com/stealthrocket/net/wasip1.
 	_ "github.com/stealthrocket/net/mysql"
 )
 
@@ -16,9 +18,12 @@ func TestMySQL(t *testing.T) {
 	}
 	defer db.Close()
 
-	r, err := db.Query("select version()")
-	if err != nil {
+	row := db.QueryRow("select version()")
+	ver := ""
+	if err := row.Scan(&ver); err != nil {
 		t.Fatal(err)
 	}
-	_ = r
+	if ver != "8.0.33" {
+		t.Errorf("wrong version returned by the mysql server: %q", ver)
+	}
 }
