39927a56903529abff1dbdab849f344d3890be26
Achille Roussel <achille.roussel@gmail.com>
2023-06-09 09:35:49 -0700

    add websocket test
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

---
+++ websocket/websocket.go
@@ -0,0 +1,8 @@
+// Package websocket provides an example of a WebSocket client and server
+// running in a program compiled to GOOS=wasip1.
+//
+// The test uses https://pkg.go.dev/nhooyr.io/websocket and relies on the
+// github.com/stealthrocket/net/http package to configure the default http
+// transport, as well as github.com/stealthrocket/wasip1 to create the server
+// listener accepting connections.
+package websocket
