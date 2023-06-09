c13a404b8c04972db524259f25af08c1cf5967a8
Achille Roussel <achille.roussel@gmail.com>
2023-06-09 11:43:15 -0700

    add mysql test
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- mysql/mysql_wasip1_test.go
+++ mysql/mysql_wasip1_test.go
@@ -1,3 +1,24 @@
 //go:build wasip1
 
 package mysql_test
+
+import (
+	"database/sql"
+	"testing"
+
+	_ "github.com/stealthrocket/net/mysql"
+)
+
+func TestMySQL(t *testing.T) {
+	db, err := sql.Open("mysql", "root:test@tcp(localhost:3306)/test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	r, err := db.Query("select version()")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = r
+}
