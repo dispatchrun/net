9d27ec51087ad7edc665fd2ca4228c62565f3e4b
Achille Roussel <achille.roussel@gmail.com>
2023-06-09 16:51:45 -0700

    add memcache test
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

---
+++ memcache/go.mod
@@ -0,0 +1,10 @@
+module github.com/stealthrocket/net/memcache
+
+go 1.20
+
+require (
+	github.com/bradfitz/gomemcache v0.0.0-20230124162541-5f7a7d875746
+	github.com/stealthrocket/net v0.1.2
+)
+
+replace github.com/bradfitz/gomemcache => github.com/mar4uk/gomemcache v0.0.0-20221107102512-3adaeb8afff3
---
+++ memcache/memcache_wasip1_test.go
@@ -0,0 +1,21 @@
+//go:build wasip1
+
+package memcache_test
+
+import (
+	"testing"
+
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/stealthrocket/net/wasip1"
+)
+
+func TestMemcache(t *testing.T) {
+	client := memcache.New("localhost:11211")
+	// Change the dial function so the client uses the WASI socket extensions
+	// missing from Go 1.21.
+	client.DialContext = wasip1.DialContext
+
+	if err := client.Ping(); err != nil {
+		t.Fatal(err)
+	}
+}
